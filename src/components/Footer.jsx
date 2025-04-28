525d03fe9e8d22107d15d0781a6589ed0e9f34d9
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-04-28 12:53:29 +0200

    Refactor Footer component and update styles; enhance layout and link visibility in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,28 @@
 export default function Footer() {
-    return (
-        <>
-
-            <footer className="py-3 my-4">
-                <div className="container">
-                    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Contact</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
-                    </ul>
-                    <p className="text-center text-body-secondary">© 2025 Booroad, Inc</p>
-                </div>
-            </footer>
-
-        </>
-
-    )
+  return (
+    <footer className="footer-custom">
+      <div className="container">
+        <ul className="nav justify-content-center pb-3 mb-3">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-white">
+              <i className="bi bi-house-door me-1"></i>Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-white">
+              <i className="bi bi-envelope me-1"></i>Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-white">
+              <i className="bi bi-info-circle me-1"></i>About
+            </a>
+          </li>
+        </ul>
+        <p className="text-center text-white">
+          © 2025 Booroad <i className="bi bi-airplane"></i>
+        </p>
+      </div>
+    </footer>
+  );
 }
